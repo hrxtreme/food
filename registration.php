5ce0edd6bad8a73994f12c90f63cf074d6e769ed
Hrushikesh Tavadarkar <hrushikesh.tavadarkar@ubisoft.com>
2020-02-03 19:04:51 +0530

    Registration page validation done
    validation on register page
    
    Co-Authored-By: nehapenshanwar <nehapenshanwar@users.noreply.github.com>

--- registration.php
+++ registration.php
@@ -4,77 +4,116 @@
 
 session_start(); //temp session
 error_reporting(0); // hide undefine index
+
 include("connection/connect.php"); // connection
-if(isset($_POST['submit'] )) //if submit btn is pressed
-{
-     if(empty($_POST['firstname']) ||  //fetching and find if its empty
-   	    empty($_POST['lastname'])|| 
-		empty($_POST['email']) ||  
-		empty($_POST['phone'])||
-		empty($_POST['password'])||
-		empty($_POST['cpassword']) ||
-		empty($_POST['cpassword']))
-		{
-			$message = "All fields must be Required!";
-		}
-	else
-	{
-		//cheching username & email if already present
-	$check_username= mysqli_query($db, "SELECT username FROM users where username = '".$_POST['username']."' ");
-	$check_email = mysqli_query($db, "SELECT email FROM users where email = '".$_POST['email']."' ");
-		
 
-	
-	if($_POST['password'] != $_POST['cpassword']){  //matching passwords
-       	$message = "Password not match";
+
+
+
+
+if(isset($_POST['submit'] )) {
+  
+    if(empty($_POST['firstname']) || empty($_POST['lastname']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['password'])|| empty($_POST['cpassword']) ) {
+      $message = 'Please fill mentioned details: ';
+      $appendM = ''; $space = ' , ';
+      $fn = 'First Name'; $ln = 'Last Name'; $e = 'Email'; $p = 'Phone Number'; $pass = 'Password'; $cpass = 'Confirm Password';
+      $u = 'Username';
+
+      if(empty($_POST['username'])){
+        if(empty($appendM)){
+          $appendM = $appendM.$u;  
+        } 
+      }
+
+      if(empty($_POST['firstname'])){
+        if(empty($appendM)){
+          $appendM = $appendM.$fn;  
+        } else {
+          $appendM = $appendM.", ".$fn;
+        }
+      } 
+
+      if(empty($_POST['lastname'])){
+        if(empty($appendM)){
+          $appendM = $appendM.$ln;  
+        } else {
+          $appendM = $appendM.", ".$ln;
+        }
+      }
+
+      if(empty($_POST['email'])){
+        if(empty($appendM)){
+          $appendM = $appendM.$e;  
+        } else {
+          $appendM = $appendM.", ".$e;
+        }
+      }
+
+      if(empty($_POST['phone'])){
+        if(empty($appendM)){
+          $appendM = $appendM.$p;  
+        } else {
+          $appendM = $appendM.", ".$p;
+        }
+      }
+
+      if(empty($_POST['password'])){
+        if(empty($appendM)){
+          $appendM = $appendM.$pass;  
+        } else {
+          $appendM = $appendM.", ".$pass;
+        }
+      }
+
+      if(empty($_POST['cpassword'])){
+        if(empty($appendM)){
+          $appendM = $appendM.$cpass;  
+        } else {
+          $appendM = $appendM.", ".$cpass;
+        }
+      }
+      $message = $message.$appendM;
+    }  else {
+      $message = '';
+      
+      if(strlen($_POST['username']) < 4) {
+        $message = 'Username should be of atleast 4 characters';
+      } elseif (!preg_match('/^[A-Za-z][A-Za-z0-9]{7,19}$/', $_POST['username'])) {
+        $message = 'Please enter valid username';
+      } elseif(mysqli_num_rows(mysqli_query($db, "SELECT username FROM users where username = '".$_POST['username']."' ")) > 0) {
+        $message = 'Username taken! Please choose a different one.';
+      } elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))  {
+        $message = "Invalid Email Address";
+      } /*elseif (!preg_match('^[\w.+\-]+@ubisoft\.com$', $_POST['email'] ) ) {
+        $message = 'Please enter Ubisoft Email Address';
+      } */elseif(mysqli_num_rows(mysqli_query($db, "SELECT email FROM users where email = '".$_POST['email']."' ")) > 0)  {
+        $message = 'Email is already registered';
+      }  /*elseif(!preg_match('^[789]\d{9}$', $_POST['phone'])) { 
+        $message = "Enter a valid phone number";
+      }  elseif(!preg_match('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$', $_POST['password'])) { 
+        $message = "Enter Valid Password: Minimum 8 characters, at least one uppercase letter, one lowercase letter, one number and one special character:";
+      } */elseif($_POST['password'] != $_POST['cpassword']){ 
+        $message = "Password must match";
+      }else {
+        $mql = "INSERT INTO users(username,f_name,l_name,email,phone,password,address) VALUES('".$_POST['username']."','".$_POST['firstname']."','".$_POST['lastname']."','".$_POST['email']."','".$_POST['phone']."','".md5($_POST['password'])."','".$_POST['address']."')";
+        mysqli_query($db, $mql);
+        $success = "Account Created successfully! <p>You will be redirected in <span id='counter'>5</span> second(s).</p>
+                            <script type='text/javascript'>
+                            function countdown() {
+                              var i = document.getElementById('counter');
+                              if (parseInt(i.innerHTML)<=0) {
+                                location.href = 'login.php';
+                              }
+                              i.innerHTML = parseInt(i.innerHTML)-1;
+                            }
+                            setInterval(function(){ countdown(); },1000);
+                            </script>'";
+  
+      header("refresh:5;url=login.php"); // redireted once inserted success
+      }
+  
     }
-	elseif(strlen($_POST['password']) < 6)  //cal password length
-	{
-		$message = "Password Must be >=6";
-	}
-	elseif(strlen($_POST['phone']) < 10)  //cal phone length
-	{
-		$message = "invalid phone number!";
-	}
-
-    elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) // Validate email address
-    {
-       	$message = "Invalid email address please type a valid email!";
-    }
-	elseif(mysqli_num_rows($check_username) > 0)  //check username
-     {
-    	$message = 'username Already exists!';
-     }
-	elseif(mysqli_num_rows($check_email) > 0) //check email
-     {
-    	$message = 'Email Already exists!';
-     }
-	else{
-       
-	 //inserting values into db
-	$mql = "INSERT INTO users(username,f_name,l_name,email,phone,password,address) VALUES('".$_POST['username']."','".$_POST['firstname']."','".$_POST['lastname']."','".$_POST['email']."','".$_POST['phone']."','".md5($_POST['password'])."','".$_POST['address']."')";
-	mysqli_query($db, $mql);
-		$success = "Account Created successfully! <p>You will be redirected in <span id='counter'>5</span> second(s).</p>
-														<script type='text/javascript'>
-														function countdown() {
-															var i = document.getElementById('counter');
-															if (parseInt(i.innerHTML)<=0) {
-																location.href = 'login.php';
-															}
-															i.innerHTML = parseInt(i.innerHTML)-1;
-														}
-														setInterval(function(){ countdown(); },1000);
-														</script>'";
-		
-		
-		
-		
-		 header("refresh:5;url=login.php"); // redireted once inserted success
-    }
-	}
-
-}
-
+  }
 
 ?>
 
@@ -110,85 +149,91 @@ if(isset($_POST['submit'] )) //if submit btn is pressed
                   <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/Ubieats_long.png" alt="" height= "50" width="150"> </a>
                   <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
                      <ul class="nav navbar-nav">
-							<li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+              <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
                             <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li>
                             
-							<?php
-						if(empty($_SESSION["user_id"]))
-							{
-								echo '<li class="nav-item"><a href="login.php" class="nav-link active">Login</a> </li>
-							  <li class="nav-item"><a href="registration.php" class="nav-link active">Signup</a> </li>';
-							}
-						else
-							{
-									
-									
-										echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">Your Orders</a> </li>';
-									echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">Logout</a> </li>';
-							}
+              <?php
+            if(empty($_SESSION["user_id"]))
+              {
+                echo '<li class="nav-item"><a href="login.php" class="nav-link active">login</a> </li>
+                <li class="nav-item"><a href="registration.php" class="nav-link active">signup</a> </li>';
+              }
+            else
+              {
+                  
+                  
+                    echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">your orders</a> </li>';
+                  echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">logout</a> </li>';
+              }
 
-						?>
-							 
+            ?>
+               
                         </ul>
                   </div>
                </div>
             </nav>
             <!-- /.navbar -->
          </header>
-         <div class="page-wrapper">
+            <section class="contact-page inner-page">
+                <div class="page-wrapper">
             <div class="breadcrumb">
                <div class="container">
                   <ul>
                      <li><a href="#" class="active">
-					  <span style="color:red;"><?php echo $message; ?></span>
-					   <span style="color:green;">
-								<?php echo $success; ?>
-										</span>
-					   
-					</a></li>
+            <span style="color:red;"><?php echo $message; ?></span>
+             <span style="color:green;">
+                <?php echo $success; ?>
+                    </span>
+             
+          </a></li>
                     
                   </ul>
                </div>
             </div>
-            <section class="contact-page inner-page">
-               <div class="container">
+              <div class="container">
+
                   <div class="row">
                      <!-- REGISTER -->
                      <div class="col-md-8">
                         <div class="widget">
                            <div class="widget-body">
                               
-							  <form action="" method="post">
+                              <form action="" method="post">
                                  <div class="row">
-								  <div class="form-group col-sm-12">
-                                       <label for="exampleInputEmail1">User Name</label>
-                                       <input class="form-control" type="text" name="username" id="example-text-input" placeholder="Ubisoft User Name"> 
+                                    <div class="form-group col-sm-12">
+                                       <label for="exampleInputEmail1">Username</label>
+                                       <input class="form-control" type="text" name="username" id="example-text-input" placeholder="Ubisoft User Name" value="<?php echo isset($_POST["username"]) ? $_POST["username"] : ''; ?>"> 
                                     </div>
                                     <div class="form-group col-sm-6">
                                        <label for="exampleInputEmail1">First Name</label>
-                                       <input class="form-control" type="text" name="firstname" id="example-text-input" placeholder="First Name"> 
+                                       <input class="form-control" type="text" name="firstname" id="example-text-input" placeholder="First Name" value="<?php echo isset($_POST["firstname"]) ? $_POST["firstname"] : ''; ?>"> 
                                     </div>
                                     <div class="form-group col-sm-6">
                                        <label for="exampleInputEmail1">Last Name</label>
-                                       <input class="form-control" type="text" name="lastname" id="example-text-input-2" placeholder="Last Name"> 
+                                       <input class="form-control" type="text" name="lastname" id="example-text-input-2" placeholder="Last Name" value="<?php echo isset($_POST["lastname"]) ? $_POST["lastname"] : ''; ?>"> 
                                     </div>
                                     <div class="form-group col-sm-6">
                                        <label for="exampleInputEmail1">Email address</label>
-                                       <input type="text" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Ubisoft email"> <small id="emailHelp" class="form-text text-muted">We"ll never share your email with anyone else.</small> 
+                                       <input type="text" class="form-control" name="email" onblur="validateEmail(this);" id="exampleInputEmail1" 
+                                       value="<?php echo isset($_POST["email"]) ? $_POST["email"] : ''; ?>"
+                                       aria-describedby="emailHelp" placeholder="Enter Ubisoft email"> <small id="emailHelp" class="form-text text-muted">We"ll never share your email with anyone else.</small> 
                                     </div>
                                     <div class="form-group col-sm-6">
                                        <label for="exampleInputEmail1">Phone number</label>
-                                       <input class="form-control" type="text" name="phone" id="example-tel-input-3" placeholder="Contact Number"> <small class="form-text text-muted">We"ll never share your email with anyone else.</small> 
+                                       <input class="form-control only-numeric" type="text" name="phone" maxlength="10"   id="example-tel-input-3" 
+                                        value="<?php echo isset($_POST["phone"]) ? $_POST["phone"] : ''; ?>"
+                                       placeholder="Contact Number"> <small class="form-text text-muted">We"ll never share your phone number with anyone else.</small> 
                                     </div>
                                     <div class="form-group col-sm-6">
                                        <label for="exampleInputPassword1">Password</label>
-                                       <input type="password" class="form-control" name="password" id="exampleInputPassword1" placeholder="Password"> 
+                                       <input type="password" class="form-control" name="password" id="exampleInputPassword1"
+                                        placeholder="Password"> 
                                     </div>
                                     <div class="form-group col-sm-6">
                                        <label for="exampleInputPassword1">Repeat password</label>
-                                       <input type="password" class="form-control" name="cpassword" id="exampleInputPassword2" placeholder="Password"> 
+                                       <input type="password" class="form-control" name="cpassword" id="exampleInputPassword2" placeholder="Password" > 
                                     </div>
-									 <div class="form-group col-sm-12">
+                   <div class="form-group col-sm-12">
                                        <label for="exampleTextarea">Delivery Address</label>
                                        <textarea class="form-control" id="exampleTextarea" placeholder="Emp ID,&#10;Project Name,&#10;Floor No,&#10;Sub Team&#10;" name="address" rows="5"></textarea>
                                     </div>
@@ -202,7 +247,7 @@ if(isset($_POST['submit'] )) //if submit btn is pressed
                                  </div>
                               </form>
                            
-						   </div>
+               </div>
                            <!-- end: Widget -->
                         </div>
                         <!-- /REGISTER -->
@@ -383,6 +428,34 @@ if(isset($_POST['submit'] )) //if submit btn is pressed
     <script src="js/headroom.js"></script>
     <script src="js/foodpicky.min.js"></script>
 
+<script type="text/javascript">
+    
+    $(document).ready(function() {
+      $(".only-numeric").bind("keypress", function (e) {
+          var keyCode = e.which ? e.which : e.keyCode
+               
+          if (!(keyCode >= 48 && keyCode <= 57)) {
+            $(".error").css("display", "inline");
+            return false;
+          }else{
+            $(".error").css("display", "none");
+          }
+      });
+    });
+     
+	 function validateEmail(emailField){
+        var reg = /.+@(ubisoft)\.com$/;
+
+        if (reg.test(emailField.value) == false) 
+        {
+            alert('Invalid Email Address');
+            return false;
+        }
+
+        return true;
+
+}
+</script>
 </body>
 
 </html>
